9574fe1ded689f5641f543e882c719e64496aa36
arpan8925 <sbxp1966@gmail.com>
2024-06-17 19:12:50 +0600

    Completed Project

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import datetime as dt
 import random
 import os
+import smtplib
 
 now = dt.datetime.now()
 current_date = now.date()
@@ -10,27 +11,42 @@ current_date = now.date()
 current_month = current_date.month
 current_day = current_date.day
 
+my_email = "sbxp1966@gmail.com"
+password = "gogatmuwcjufqlet"
+
 
 birthday_dates = pd.read_csv("birthdays.csv")
+           
+birthday_check = birthday_dates[(birthday_dates['month'] == current_month) & (birthday_dates['day'] == current_day)]
 
-def check_if_birthday():
-    global birthday_check           
-    birthday_check = birthday_dates[(birthday_dates['month'] == current_month) & (birthday_dates['day'] == current_day)]
-    return birthday_check
+if not birthday_check.empty:
+    for index, row in birthday_check.iterrows():
+        random_file = random.choice(os.listdir("letter_templates"))
+        with open(f"letter_templates/{random_file}", "r") as letter:
+            letter_text = letter.read()
 
-while check_if_birthday == True:
-    if not birthday_check.empty:
-        for index, row in birthday_check.iterrows():
-            random_file = random.choice(os.listdir("letter_templates"))
-            with open(f"letter_templates/{random_file}", "r") as letter:
-                letter_text = letter.read()
-
-        birthday_wish = letter_text.replace("[NAME]", row['name'])
-        print(birthday_wish)
+    birthday_wish = letter_text.replace("[NAME]", row['name'])
+    
 
 
+with smtplib.SMTP("smtp.gmail.com") as smtp:
+    smtp.starttls()
+    smtp.login(user=my_email, password=password)
+    smtp.sendmail(
+        from_addr=my_email,
+        to_addrs= row['email'],
+        msg=f"Subject: Happy Birthday !\n\n{birthday_wish}"
+    )
 
-# 4. Send the letter generated in step 3 to that person's email address.
+with smtplib.SMTP("smtp.gmail.com") as smtp:
+    smtp.starttls()
+    smtp.login(user=my_email, password=password)
+    smtp.sendmail(
+        from_addr=my_email,
+        to_addrs=my_email,
+        msg=f"Subject: Birthday Notification\n\nHeads UP! Today is {row['name']} 's Birthday. Your BOT mailed {row['name']} already, But Wish {row['name']} personally"
+        
+    )
 
 
 
